a54609175adbd96f013b70dc51014b9799ae5e70
Cosmo Wolfe <cosmo.wolfe@gmail.com>
2017-06-26 17:03:53 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# slack-cleaner
+
+Script to remove dead slack channels. 
+
+## Usage
+
+Get a slack token from https://api.slack.com/custom-integrations/legacy-tokens
+
+```
+npm install
+SLACK_TOKEN=redacted ./node_modules/.bin/ts-node index.ts
+```
---
+++ index.ts
@@ -0,0 +1,28 @@
+import * as slack from 'slack-promise';
+
+const token = process.env.SLACK_TOKEN;
+const EMPTY_MEMBERS = 2;
+const ARCHIVE_MESSAGE = "Archiving this channel due to lack of activity. Feel free to un-archive if you feel this was done in error.";
+
+(async () => {
+  const channels = (await slack.channels.list({token})).channels.filter(c => !c.is_archived);
+
+  for (let i = 0; i < channels.length; i++) { 
+    const c = channels[i];
+    console.log(`#${c.name} - ${i}/${channels.length}`);
+    let kill = false;
+    if (c.num_members <= EMPTY_MEMBERS) {
+      kill = true;
+    } else {
+    const messages = (await slack.channels.history({token, channel: c.id})).messages.filter(m => m.type === "message" && (!m.subtype || m.subtype == "bot_message"));
+    if(messages.length === 0) {
+      kill = true;
+    };
+    if (kill) {
+      console.log(`Archiving #${c.name}`);
+      await slack.channels.join({token, name: `#${c.name}`});
+      await slack.chat.postMessage({as_user: true, token, channel: c.id, text: ARCHIVE_MESSAGE});
+      await slack.channels.archive({token, channel: c.id});
+    }
+  }
+}})();
